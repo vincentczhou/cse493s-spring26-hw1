171a90beb8f8438f7e0a81a2a17837bc1c1795b6
Vincent <43220811+vincentczhou@users.noreply.github.com>
2026-04-24 19:30:40 -0700

    fix: enable last token only prediction/encapsulate word-level tokenizer (#3)
    Co-authored-by: Vincent Chau <vkchau@cs.washington.edu>

--- utils.py
+++ utils.py
@@ -2,8 +2,6 @@ from pathlib import Path
 
 import torch
 from torch.utils.data import Dataset
-# import yaml
-
 
 UNK = "<UNK>"
 PAD = "<PAD>"
@@ -15,15 +13,6 @@ BOS_IDX = 2
 EOS_IDX = 3
 
 
-# def load_config(config_path: Path) -> tuple[dict, GPTConfig]:
-#     c = GPTConfig()
-#     with open(config_path, "r") as f:
-#         config_dict = yaml.safe_load(f)
-#     for key, value in config_dict["gptconfig"].items():
-#         setattr(c, key, value)
-#     return config_dict, c
-
-
 def load_data(data_path: Path) -> list[str]:
     data = []
     with open(data_path, "r") as f:
@@ -31,54 +20,65 @@ def load_data(data_path: Path) -> list[str]:
     return data
 
 
-def train_word_tokenizer(data: list[str], vocab_size: int) -> dict[str, int]:
-    # Why word level tokenizer? Because that's what the grokking paper does!
-    # Create a set of unique tokens
-    unique_tokens = set()
-    for line in data:
-        tokens = line.split()  # Split by whitespace
-        unique_tokens.update(tokens)
+class Tokenizer:
+    def __init__(self, data: list[str], vocab_size: int):
+        # Why word level tokenizer? Because that's what the grokking paper does!
+        # Create a set of unique tokens
+        unique_tokens = set()
+        for line in data:
+            tokens = line.split()  # Split by whitespace
+            unique_tokens.update(tokens)
 
-    # TODO: Limit the vocabulary size, this should be done more intelligently in practice (e.g., by frequency), but for simplicity, we'll just take the first `vocab_size` tokens because for the task datasets our vocab size will never be that big.
-    if len(unique_tokens) > vocab_size:
-        unique_tokens = set(list(unique_tokens)[:vocab_size])
+        # TODO: Limit the vocabulary size, this should be done more intelligently in practice (e.g., by frequency), but for simplicity, we'll just take the first `vocab_size` tokens because for the task datasets our vocab size will never be that big.
+        if len(unique_tokens) > vocab_size:
+            unique_tokens = set(list(unique_tokens)[:vocab_size])
 
-    # Create a token to index mapping
-    token_to_index = {
-        token: idx for idx, token in enumerate(unique_tokens, start=4)
-    }  # Start indexing from 4 to reserve 0-3 for special tokens
-    token_to_index[UNK] = UNK_IDX
-    token_to_index[PAD] = PAD_IDX
-    token_to_index[BOS] = BOS_IDX
-    token_to_index[EOS] = EOS_IDX
+        # Create a token to index mapping
+        # Start indexing from 4 to reserve 0-3 for special tokens
+        self.token_to_index = {
+            token: idx for idx, token in enumerate(unique_tokens, start=4)
+        }
+        self.token_to_index[UNK] = UNK_IDX
+        self.token_to_index[PAD] = PAD_IDX
+        self.token_to_index[BOS] = BOS_IDX
+        self.token_to_index[EOS] = EOS_IDX
+        self.index_to_token = {v: k for k, v in self.token_to_index.items()}
 
-    return token_to_index
+    def encode(self, s: str) -> list[int]:
+        return (
+            [BOS_IDX]
+            + [self.token_to_index.get(t, UNK_IDX) for t in s.split()]
+            + [EOS_IDX]
+        )
 
+    def decode(self, indices: list[int]) -> str:
+        # skip = {BOS_IDX, EOS_IDX, PAD_IDX}
+        # return ' '.join(self.index_to_token[i] for i in indices if i not in skip)
+        return " ".join(self.index_to_token[i] for i in indices)
 
-def tokenize(data: list[str], token_to_index: dict[str, int]) -> list[int]:
-    tokenized_data = []
-    for line in data:
-        tokens = line.split()  # Split by whitespace
-        token_indices = [
-            token_to_index.get(token, token_to_index[UNK]) for token in tokens
-        ]
-        token_indices = [token_to_index[BOS]] + token_indices + [token_to_index[EOS]]
-        tokenized_data.append(token_indices)
-    return tokenized_data
+    def __len__(self):
+        return len(self.token_to_index)
 
 
 class TextDataset(Dataset):
-    def __init__(self, samples: list[str], token_to_index: dict[str, int]):
-        self.tokenized_data = torch.tensor(tokenize(samples, token_to_index))
+    def __init__(
+        self, samples: list[str], tokenizer: Tokenizer, last_token_only: bool = False
+    ):
+        self.tokenized_data = [
+            torch.tensor(tokenizer.encode(s), dtype=torch.long) for s in samples
+        ]
+        self.last_token_only = last_token_only
 
     def __len__(self):
         return len(self.tokenized_data)
 
     def __getitem__(self, idx):
         tokens = self.tokenized_data[idx]
-        return tokens[:-1], tokens[
-            1:
-        ]  # Input sequence and target sequence (shifted by one)
+        x = tokens[:-1]
+        y = tokens[1:].clone()
+        if self.last_token_only:
+            y[:-1] = -100
+        return x, y
 
     def collate_fn(batch):
         xs, ys = zip(*batch)
